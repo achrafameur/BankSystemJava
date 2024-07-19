1cb31a204620838644284253c7b23160e3f244f7
Achraf Ameur <72624148+achrafameur@users.noreply.github.com>
2024-07-19 14:05:02 +0200

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,41 @@
+public abstract class BankAccount {
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolder, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+    }
+
+    // Getter and Setter methods
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // Abstract methods
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+    public abstract void displayAccountInfo();
+}
